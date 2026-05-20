f13d1df03e834742f6c2b2aafad82b185296c8f8
Aditya <adityarajsir162@gmail.com>
2026-05-20 12:29:47 +0530

    style: force light theme natively in android

--- android/app/src/main/java/com/countdowntimer/app/MainApplication.kt
+++ android/app/src/main/java/com/countdowntimer/app/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import androidx.appcompat.app.AppCompatDelegate
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -48,6 +49,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.

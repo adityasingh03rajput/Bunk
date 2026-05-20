90e76f526f9975c91585fc200d79dbc3151c64ae
Aditya <adityarajsir162@gmail.com>
2026-05-20 12:32:36 +0530

    revert: remove global light mode lock to allow dark mode in rest of app

--- android/app/src/main/java/com/countdowntimer/app/MainApplication.kt
+++ android/app/src/main/java/com/countdowntimer/app/MainApplication.kt
@@ -11,7 +11,6 @@ import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import androidx.appcompat.app.AppCompatDelegate
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -49,7 +48,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
